5e01f3bdad724b3250e602c6b8ff2c63114c6080
David <dsi28@hotmail.com>
2018-05-05 22:58:50 -0400

    Made a couple of changes to the code.

--- app/src/main/java/com/example/android/soccergamestatistics/MainActivity.java
+++ app/src/main/java/com/example/android/soccergamestatistics/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.android.soccergamestatistics;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,18 +6,30 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
-    int scoreA = 0, scoreB = 0, foulA = 0, foulB = 0, tempSA = scoreA, tempSB = scoreB, tempFA = foulA, tempFB = foulB;
+    int scoreA = 0;
+    int scoreB = 0;
+    int foulA = 0;
+    int foulB = 0;
+    int tempSA = scoreA;
+    int tempSB = scoreB;
+    int tempFA = foulA;
+    int tempFB = foulB;
     boolean justReset = false;
     String lastUpdate = "";
-
+    TextView scoreTVB;
+    TextView foulBTV;
+    TextView foulTV;
+    TextView scoreTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scoreTVB = (TextView) findViewById(R.id.scoreB);
+        foulBTV = (TextView) findViewById(R.id.foulB);
+        foulTV = (TextView) findViewById(R.id.foulA);
+        scoreTV = (TextView) findViewById(R.id.scoreA);
     }
-
     public void goalA(View view) {
         tempSA = scoreA;
         scoreA++;
@@ -26,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         justReset = false;
         lastUpdate = "ga";
     }
-
     public void goalB(View view) {
         tempSB = scoreB;
         scoreB++;
@@ -34,16 +44,11 @@ public class MainActivity extends AppCompatActivity {
         justReset = false;
         lastUpdate = "gb";
     }
-
     private void updateScoreB(int scoreB) {
-        TextView scoreTVB = (TextView) findViewById(R.id.scoreB);
         scoreTVB.setText("" + scoreB);
     }
-
-
     public void anulA(View view) {
         if (scoreA == 0) {
-
         } else {
             tempSA = scoreA;
             scoreA--;
@@ -52,10 +57,8 @@ public class MainActivity extends AppCompatActivity {
         }
         justReset = false;
     }
-
     public void anulB(View view) {
         if (scoreB == 0) {
-
         } else {
             tempSB = scoreB;
             scoreB--;
@@ -64,8 +67,6 @@ public class MainActivity extends AppCompatActivity {
         }
         justReset = false;
     }
-
-
     public void addFoulA(View view) {
         tempFA = foulA;
         foulA++;
@@ -73,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
         justReset = false;
         lastUpdate = "fa";
     }
-
     public void addFoulB(View view) {
         tempFB = foulB;
         foulB++;
@@ -81,9 +81,7 @@ public class MainActivity extends AppCompatActivity {
         justReset = false;
         lastUpdate = "fb";
     }
-
     public void minusFoulB(View view) {
-
         if (foulB == 0) {
         } else {
             tempFB = foulB;
@@ -93,15 +91,11 @@ public class MainActivity extends AppCompatActivity {
         }
         justReset = false;
     }
-
     private void updateFoulB(int foulB) {
-        TextView foulBTV = (TextView) findViewById(R.id.foulB);
         foulBTV.setText("" + foulB);
     }
-
     public void minusFoulA(View view) {
         if (foulA == 0) {
-
         } else {
             tempFA = foulA;
             foulA--;
@@ -110,19 +104,12 @@ public class MainActivity extends AppCompatActivity {
         }
         justReset = false;
     }
-
     private void updateFoul(int foulA) {
-        TextView foulTV = (TextView) findViewById(R.id.foulA);
         foulTV.setText("" + foulA);
     }
-
-
     private void updateScoreA(int score) {
-        TextView scoreTV = (TextView) findViewById(R.id.scoreA);
         scoreTV.setText("" + score);
     }
-
-
     public void reset(View view) {
         tempSA = scoreA;
         tempSB = scoreB;
@@ -132,8 +119,6 @@ public class MainActivity extends AppCompatActivity {
         updateAll(scoreA, scoreB, foulA, foulB);
         justReset = true;
     }
-
-
     public void undo(View view) {
         if (justReset == true) {
             scoreA = tempSA;
@@ -157,12 +142,10 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
     private void updateAll(int sA, int sB, int fA, int fB) {
         updateScoreA(sA);
         updateScoreB(sB);
         updateFoul(fA);
         updateFoulB(fB);
     }
-
 }
